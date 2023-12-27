befb76e14c792e7c35a4ec16f24cba84137b8583
Raunak Joshi <raunaksjoshis101@gmail.com>
2023-12-27 13:53:07 -0800

    adding links into nav bar

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
     <div>
+      <div className="container">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/contacts">Contact</Link>
+        </li>
+        <li>
+          <Link to="/ProjectsDetails">ProjectDetails</Link>
+        </li>
+        <li>
+          <Link to="/ProjectsShowcase">ProjectShowcase</Link>
+        </li>
+      </div>
       <h1>This is my home page</h1>
+      <div> home page contents</div>
     </div>
   );
 };
