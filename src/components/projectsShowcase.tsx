67ba10ee6a6655be492d20c42d498994f2d087e5
Lakeshwar <110439492+lakeshwarmandal@users.noreply.github.com>
2023-12-28 15:41:47 -0600

    Somthing Change

--- src/components/projectsShowcase.tsx
+++ src/components/projectsShowcase.tsx
@@ -1,10 +1,22 @@
 import Navbar from "./Navbar";
+import "./ProjectsShowcase.css";
 
 const ProjectsShowcase = () => {
   return (
-    <div>
+    <div className="nav">
       <Navbar />
-      <h1>This is my projectsShowcase</h1>
+      <div className="a">aaaa</div>
+      <div className="b">aaaaa</div>
+      <div className="c">aaaa</div>
+      <div className="d">aaaaa</div>
+      <h1 className="h1">Lakeshwar projectsShowcase</h1>
+      <img
+        className="imge"
+        src="/public/jyoti.png"
+        alt="img"
+        height={200}
+        width={350}
+      />
     </div>
   );
 };
